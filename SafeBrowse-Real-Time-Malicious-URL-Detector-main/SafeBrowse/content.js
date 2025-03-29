05078d4fbb6db777900a422036bf626810ed2847
Basant Rauniyar <145216407+basantrauniyar15@users.noreply.github.com>
2025-03-29 18:21:16 +0530

    Delete SafeBrowse-Real-Time-Malicious-URL-Detector-main directory

--- SafeBrowse-Real-Time-Malicious-URL-Detector-main/SafeBrowse/content.js
+++
@@ -1,9 +0,0 @@
-let url = window.location.hostname;
-
-fetch(chrome.runtime.getURL("blocklist.json"))
-  .then(response => response.json())
-  .then(data => {
-    if (data.malicious_urls.includes(url)) {
-      alert("⚠️ Warning: This website is flagged as malicious!");
-    }
-  });
